4309372e188f825afc0bd3c7933616d3d369ad3b
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-10-01 01:09:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/loot_bazar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loot_bazar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/user-panel/main-screen.dart
@@ -0,0 +1,16 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber ,
+        title: Text('heading'),
+      ),
+    );
+  }
+}
